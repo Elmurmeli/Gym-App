ca7821a3e8467e5cec42ffd05a1eb17ebf531981
Elmurmeli <79470113+Elmurmeli@users.noreply.github.com>
2025-05-19 11:27:19 +0300

    Responsive UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import Home from './pages/Home';
 import LogExercise from './pages/LogExercise';
 import History from './pages/History';
 
+
 export default function App() {
   return (
+    
     <Router>
-      <div className="min-h-screen bg-gray-100 text-gray-800 p-4 max-w-2x1 mx-auto">
-        <nav className="bg-white shadow p-4 rounded flex justify-center gap-6 mb-6">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <div className="h-screen bg-gray-100 text-gray-800 p-4 w-screen mx-auto" src="...">
+        <nav className="bg-white shadow p-4 rounded flex justify-center gap-8 mb-6">
           <Link to="/" className="hover:text-blue-600 font-medium">Home</Link>
           <Link to="/log" className="hover:text-blue-600 font-medium">Log Exercise</Link>
           <Link to="/history" className="hover:text-blue-600 font-medium">History</Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
     return (
       <div>
-        <h1 className="text-2xl font-bold">🏋️ Welcome to Gym Tracker</h1>
-        <p className="mt-2">Track your workouts and monitor your progress!</p>
+        <h1 className="text-2xl font-bold flex justify-center"> Welcome to Gym Tracker</h1>
+        <p className="mt-2 flex justify-center">Track your workouts and monitor your progress!</p>
       </div>
     );
   }
