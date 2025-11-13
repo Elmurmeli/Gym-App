4f476eb9a75c78922a0ac3a5a0c59375888f5eaa
Altti Väätäinen <altti.vaatainen@gmail.com>
2025-11-13 14:00:44 +0200

    Fixed overlapping of some UI changes in css files and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <div className="h-screen bg-gray-100 text-gray-800 p-4 w-screen mx-auto" src="...">
         <nav className=" bg-white shadow p-4 rounded flex justify-between items-center mb-6">
-          <h1 class="text-blue-500 text-2xl font-bold">Gym Tracker</h1>
+          <h1 className="text-blue-500 text-2xl font-bold">Gym Tracker</h1>
           <div className="flex gap-8">
           <Link to="/" className="hover:text-blue-600 font-medium">Home</Link>
           <Link to="/logs" className="hover:text-blue-600 font-medium">Log Exercise</Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,32 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
     return (
-      <div class="max-w-4xl mx-auto p-6">
-        <h1 className="text-2xl font-bold flex justify-center"> Welcome to Gym Tracker</h1>
-        <p className="mt-2 flex justify-center">Track your workouts and monitor your progress!</p>
+      <div className="max-w-4xl mx-auto p-6">
+        {/* Hero section */}
+        <h1 className="text-2xl font-bold flex justify-center">
+          Welcome to Gym Tracker
+        </h1>
+        <p className="mt-2 flex justify-center">
+          Track your workouts, monitor your progress, and stay motivated to reach your fitness goals!
+        </p>
+        <div className="flex justify-center gap-4 mb-12">
+
+          <Link 
+          to="/logs" 
+          className="bg-blue-600 text-white font-semibold px-6 py-3 rounded-lg hover:bg-blue-700 transition shadow-md hover:shadow-lg">
+            Log Exercise
+          </Link>
+
+          <Link
+          to="/history"
+          className="bg-gray-300 text-gray-800 font-semibold px-6 py-3 rounded-lg hover:bg-gray-400 transition"
+          >View History
+          </Link>
+
+          
+        </div>
+
       </div>
     );
   }
