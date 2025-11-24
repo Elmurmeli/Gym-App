1d4268eb81cc86bd57db1fcd8b8e2597ba7a2acb
Altti Väätäinen <altti.vaatainen@gmail.com>
2025-11-24 15:27:56 +0200

    Redirect to home page on logout

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ export default function App() {
 
   const handleLogout = async () => {
     await supabase.auth.signOut();
+    window.location.href = "/"; // Redirect to home on logout
   };
 
   return (
