6757b60953db52a33853ba390ac05de5030d868f
Altti Väätäinen <altti.vaatainen@gmail.com>
2026-01-26 16:12:19 +0200

    Fixes to unit tests

--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
+    environment: 'happy-dom',
     globals: true,
     setupFiles: 'tests/setupTests.js',
     include: ['src/**/*.{test,spec}.{js,jsx,ts,tsx}', 'tests/unit/**/*.{test,spec}.{js,jsx,ts,tsx}'],
